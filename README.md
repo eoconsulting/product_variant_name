95cd10987fb3593784a074d41ac4ef3a3a72b3f2
Mariano Ruiz <mrsarm@gmail.com>
2013-01-05 16:34:37 -0300

    Source code of the module

---
+++ README.md
@@ -0,0 +1,21 @@
+Product Variant Name Module
+===========================
+
+This OpenERP module change how the product name is computed in a order line.
+
+If the product have variants description, use:
+
+    name_template + variants
+
+else use:
+
+    name + variants
+
+Without this module, when product have variants, the variant description
+is added twice to the name.
+
+This sources are available in https://github.com/eoconsulting/product_variant_name
+
+__________
+
+[Enterprise Objects Consulting](http://www.eoconsulting.com.ar)
---
+++ product.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2013  Enterprise Objects Consulting
+#
+#    Author: Mariano Ruiz <mrsarm@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+from osv import fields
+
+class product_product(osv.osv):
+    _name = 'product.product'
+    _inherit = 'product.product'
+
+    def name_get(self, cr, user, ids, context=None):
+        if context is None:
+            context = {}
+        if not len(ids):
+            return []
+        def _name_get(d):
+            name = d.get('name','')
+            code = d.get('default_code',False)
+            if code:
+                name = '[%s] %s' % (code,name)
+            if d.get('variants'):
+                name = name + ' - %s' % (d['variants'],)
+            return (d['id'], name)
+
+        partner_id = context.get('partner_id', False)
+
+        result = []
+        for product in self.browse(cr, user, ids, context=context):
+            sellers = filter(lambda x: x.name.id == partner_id, product.seller_ids)
+            if sellers:
+                for s in sellers:
+                    mydict = {
+                              'id': product.id,
+                              'name': s.product_name or product.name,
+                              'default_code': s.product_code or product.default_code,
+                              'variants': product.variants
+                              }
+                    result.append(_name_get(mydict))
+            else:
+                name = product.name
+                if product.variants:
+                    name = product.name_template
+                mydict = {
+                          'id': product.id,
+                          'name': name,
+                          'default_code': product.default_code,
+                          'variants': product.variants
+                          }
+                result.append(_name_get(mydict))
+        return result
